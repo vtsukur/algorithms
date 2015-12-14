31fd1d24a247d4fda4b570547f6c21eead8d101e
vtsukur <flushdia@gmail.com>
2015-12-15 01:21:09 +0200

    Simplistic implementation of Karatsuba method relying on integers, standard sum and no overflows.

--- src/main/java/org/vtsukur/algorithms/divide_and_conquer/KaratsubaMultiplication.java
+++ src/main/java/org/vtsukur/algorithms/divide_and_conquer/KaratsubaMultiplication.java
@@ -1,12 +1,43 @@
 package org.vtsukur.algorithms.divide_and_conquer;
 
+import java.util.stream.IntStream;
+
 /**
  * @author volodymyr.tsukur
  */
 public final class KaratsubaMultiplication {
 
-    public long multiply(final long first, final long second, final int n) {
-        return 7006652;
+    public int multiply(final int first, final int second, final int n) {
+        if (n == 1) {
+            return first * second;
+        }
+
+        final int halfN = n / 2;
+
+        final int tenPowerN = pow(10, n);
+        final int tenPowerHalfN = pow(10, halfN);
+
+        final int b = first % tenPowerHalfN;
+        final int a = (first - b) / tenPowerHalfN;
+        final int d = second % tenPowerHalfN;
+        final int c = (second - d) / tenPowerHalfN;
+
+        final int ac = multiply(a, c, halfN);
+        final int bd = multiply(b, d, halfN);
+
+        final int aPlusB = a + b;
+        final int cPlusD = c + d;
+        final int aPlusBcPlusD = multiply(aPlusB, cPlusD, halfN);
+        final int adPlusBc = aPlusBcPlusD - ac - bd;
+
+        return ac * tenPowerN + adPlusBc * tenPowerHalfN + bd;
+    }
+
+    private int pow(final int base, final int power) {
+        if (0 == power) {
+            return 1;
+        }
+        return IntStream.range(1, power).reduce(base, (value, i) -> value * base);
     }
 
 }
--- src/test/java/org/vtsukur/algorithms/divide_and_conquer/KaratsubaMultiplicationTest.java
+++ src/test/java/org/vtsukur/algorithms/divide_and_conquer/KaratsubaMultiplicationTest.java
@@ -10,10 +10,10 @@ public class KaratsubaMultiplicationTest {
 
     @Test
     public void fourDigitMultiplication() {
-        final long first = 1234;
-        final long second = 5678;
+        final int first = 1234;
+        final int second = 5678;
 
-        final long product = new KaratsubaMultiplication().multiply(first, second, 4);
+        final int product = new KaratsubaMultiplication().multiply(first, second, 4);
 
         Assert.assertEquals(first * second, product);
     }
