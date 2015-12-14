68417c750657ce627aba3b62090206108210ca01
vtsukur <flushdia@gmail.com>
2015-12-15 00:36:41 +0200

    Adding stubs for Karatsuba multiplication algorithm and test.

---
+++ src/main/java/org/vtsukur/algorithms/divide_and_conquer/KaratsubaMultiplication.java
@@ -0,0 +1,12 @@
+package org.vtsukur.algorithms.divide_and_conquer;
+
+/**
+ * @author volodymyr.tsukur
+ */
+public final class KaratsubaMultiplication {
+
+    public long multiply(final long first, final long second) {
+        return 7006652;
+    }
+
+}
---
+++ src/test/java/org/vtsukur/algorithms/divide_and_conquer/KaratsubaMultiplicationTest.java
@@ -0,0 +1,21 @@
+package org.vtsukur.algorithms.divide_and_conquer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author volodymyr.tsukur
+ */
+public class KaratsubaMultiplicationTest {
+
+    @Test
+    public void fourDigitMultiplication() {
+        final long first = 1234;
+        final long second = 5678;
+
+        final long product = new KaratsubaMultiplication().multiply(first, second);
+
+        Assert.assertEquals(first * second, product);
+    }
+
+}
