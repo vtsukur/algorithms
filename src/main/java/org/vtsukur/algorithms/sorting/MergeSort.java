e5943761a208c0c76bdbaa0420eeea2c431b6e58
vtsukur <flushdia@gmail.com>
2015-12-16 00:03:16 +0200

    Simplistic implementation of merge sort.

--- src/main/java/org/vtsukur/algorithms/sorting/MergeSort.java
+++ src/main/java/org/vtsukur/algorithms/sorting/MergeSort.java
@@ -1,12 +1,45 @@
 package org.vtsukur.algorithms.sorting;
 
 /**
+ * Uses array allocations for each recursive call to simplify the code.
+ * Production-ready implementation should reuse a temporary array instead.
+ *
  * @author volodymyr.tsukur
  */
-public class MergeSort {
+public final class MergeSort {
 
     public int[] sort(final int[] input) {
-        return new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+        return sort(input, 0, input.length - 1);
+    }
+
+    private int[] sort(final int[] input, final int from, final int to) {
+        final int distance = to - from + 1;
+        if (1 == distance) {
+            return new int[] { input[from] };
+        }
+        final int half = distance / 2;
+
+        final int aftermath = from + half;
+        final int rubicon = aftermath - 1;
+        final int[] left = sort(input, from, rubicon);
+        final int[] right = sort(input, aftermath, to);
+
+        final int[] result = new int[distance];
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                result[k++] = left[i++];
+            } else {
+                result[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            result[k++] = left[i++];
+        }
+        while (j < right.length) {
+            result[k++] = right[j++];
+        }
+        return result;
     }
 
 }
--- src/test/java/org/vtsukur/algorithms/sorting/MergeSortTest.java
+++ src/test/java/org/vtsukur/algorithms/sorting/MergeSortTest.java
@@ -10,11 +10,11 @@ public class MergeSortTest {
 
     @Test
     public void sort() {
-        final int[] input = { 5, 8, 1, 3, 7, 4, 6, 2 };
+        final int[] array = { 5, 8, 1, 3, 7, 4, 6, 2 };
 
-        final int[] output = new MergeSort().sort(input);
+        final int[] result = new MergeSort().sort(array);
 
-        Assert.assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, output);
+        Assert.assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, result);
     }
 
 }
