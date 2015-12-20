bc174be5eb50c34ee72567838cec967bf486da67
vtsukur <flushdia@gmail.com>
2015-12-21 00:54:49 +0200

    Enhancing merge sort to work with any object type and custom comparator.

--- src/main/java/org/vtsukur/algorithms/sorting/MergeSortAndInversionCount.java
+++ src/main/java/org/vtsukur/algorithms/sorting/MergeSortAndInversionCount.java
@@ -3,37 +3,41 @@ package org.vtsukur.algorithms.sorting;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
+import java.util.Comparator;
+
 /**
  * Uses array allocations for each recursive call to simplify the code.
  * Production-ready implementation should reuse a temporary array instead.
  *
  * @author volodymyr.tsukur
  */
-public final class MergeSortAndInversionCount {
+public final class MergeSortAndInversionCount<T> {
 
-    public Pair<int[], Long> sort(final int[] input) {
-        return sort(input, 0, input.length - 1);
+    public Pair<T[], Long> sort(final T[] input, final Comparator<T> comparator) {
+        return sort(input, comparator, 0, input.length - 1);
     }
 
-    private Pair<int[], Long> sort(final int[] input, final int from, final int to) {
+    private Pair<T[], Long> sort(final T[] input, final Comparator<T> comparator, final int from, final int to) {
         final int distance = to - from + 1;
         if (1 == distance) {
-            return new ImmutablePair<>(new int[] { input[from] }, 0L);
+            //noinspection unchecked
+            return new ImmutablePair<>((T[]) new Object[] { input[from] }, 0L);
         }
         final int half = distance / 2;
 
         final int aftermath = from + half;
         final int rubicon = aftermath - 1;
-        final Pair<int[], Long> left = sort(input, from, rubicon);
-        final int[] leftArray = left.getLeft();
-        final Pair<int[], Long> right = sort(input, aftermath, to);
-        final int[] rightArray = right.getLeft();
+        final Pair<T[], Long> left = sort(input, comparator, from, rubicon);
+        final T[] leftArray = left.getLeft();
+        final Pair<T[], Long> right = sort(input, comparator, aftermath, to);
+        final T[] rightArray = right.getLeft();
 
-        final int[] result = new int[distance];
+        @SuppressWarnings("unchecked")
+        final T[] result = (T[]) new Object[distance];
         int splitInversions = 0;
         int i = 0, j = 0, k = 0;
         while (i < leftArray.length && j < rightArray.length) {
-            if (leftArray[i] <= rightArray[j]) {
+            if (comparator.compare(leftArray[i], rightArray[j]) <= 0) {
                 result[k++] = leftArray[i++];
             } else {
                 splitInversions += leftArray.length - i;
--- src/test/java/org/vtsukur/algorithms/sorting/MergeSortAndInversionCountAndInversionCountTest.java
+++ src/test/java/org/vtsukur/algorithms/sorting/MergeSortAndInversionCountAndInversionCountTest.java
@@ -9,6 +9,9 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author volodymyr.tsukur
@@ -17,39 +20,40 @@ public class MergeSortAndInversionCountAndInversionCountTest {
 
     @Test
     public void eightNumbers() {
-        final int[] array = { 5, 8, 1, 3, 7, 4, 6, 2 };
+        final Integer[] array = { 5, 8, 1, 3, 7, 4, 6, 2 };
 
-        final Pair<int[], Long> result = new MergeSortAndInversionCount().sort(array);
+        final Pair<Integer[], Long> result = new MergeSortAndInversionCount<Integer>().sort(array, Comparator.<Integer>naturalOrder());
 
-        Assert.assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, result.getLeft());
+        Assert.assertArrayEquals(new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8 }, result.getLeft());
         Assert.assertEquals(16L, result.getRight().longValue());
     }
 
     @Test
     public void sixNumbers() {
-        final int[] array = { 1, 3, 5, 2, 4, 6 };
+        final Integer[] array = { 1, 3, 5, 2, 4, 6 };
 
-        final Pair<int[], Long> result = new MergeSortAndInversionCount().sort(array);
+        final Pair<Integer[], Long> result = new MergeSortAndInversionCount<Integer>().sort(array, Comparator.<Integer>naturalOrder());
 
-        Assert.assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6 }, result.getLeft());
+        Assert.assertArrayEquals(new Integer[] { 1, 2, 3, 4, 5, 6 }, result.getLeft());
         Assert.assertEquals(3L, result.getRight().longValue());
     }
 
     @Test
     public void oneHundredThousandNumbers() throws URISyntaxException, IOException {
-        final int[] array = Files.lines(Paths.get(getClass().getResource("100k_numbers.txt").toURI()))
+        final List<Integer> numbers = Files.lines(Paths.get(getClass().getResource("100k_numbers.txt").toURI()))
                 .filter(line -> line != null && !line.isEmpty())
-                .mapToInt(Integer::valueOf)
-                .toArray();
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
+        final Integer[] array = numbers.toArray(new Integer[numbers.size()]);
 
-        final Pair<int[], Long> result = new MergeSortAndInversionCount().sort(array);
+        final Pair<Integer[], Long> result = new MergeSortAndInversionCount<Integer>().sort(array, Comparator.<Integer>naturalOrder());
 
         // double-checking input
         Arrays.sort(array);
         final int SIZE = 100000;
         Assert.assertEquals(array.length, SIZE);
-        Assert.assertEquals(array[0], 1);
-        Assert.assertEquals(array[array.length - 1], SIZE);
+        Assert.assertEquals(array[0].intValue(), 1);
+        Assert.assertEquals(array[array.length - 1].intValue(), SIZE);
 
         Assert.assertArrayEquals(array, result.getLeft());
         Assert.assertEquals(2407905288L, result.getRight().longValue());
