0432bd7e1c1ddc4444ff9e53ce6c8b4277c58288
vtsukur <flushdia@gmail.com>
2016-01-23 17:18:03 +0200

    Parameterizing the test to make it ready for multiple union-find implementations.

--- src/test/java/org/vtsukur/algorithms/union_find/QuickFindUFTest.java
+++ src/test/java/org/vtsukur/algorithms/union_find/QuickFindUFTest.java
@@ -1,8 +1,11 @@
 package org.vtsukur.algorithms.union_find;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
+import java.util.Collection;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -10,30 +13,44 @@ import static org.hamcrest.MatcherAssert.assertThat;
 /**
  * @author volodymyr.tsukur
  */
+@RunWith(Parameterized.class)
 public class QuickFindUFTest {
+
+    private static final int N = 10;
+
+    private static final AssertableTestEntry[] ASSERTABLE_INPUT = new AssertableTestEntry[] {
+            new AssertableTestEntry(4, 3, false),
+            new AssertableTestEntry(3, 8, false),
+            new AssertableTestEntry(5, 6, false),
+            new AssertableTestEntry(9, 4, false),
+            new AssertableTestEntry(2, 1, false),
+            new AssertableTestEntry(8, 9, true),
+            new AssertableTestEntry(5, 0, false),
+            new AssertableTestEntry(7, 2, false),
+            new AssertableTestEntry(6, 1, false),
+            new AssertableTestEntry(0, 7, true),
+            new AssertableTestEntry(2, 5, true),
+            new AssertableTestEntry(8, 4, true),
+            new AssertableTestEntry(2, 3, false),
+            new AssertableTestEntry(0, 9, true),
+    };
+
+    private final QuickFindUF uf;
+
+    public QuickFindUFTest(final Object uf) {
+        this.uf = (QuickFindUF) uf;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> algorithms() {
+        return Arrays.asList(new Object[][] {
+                { new QuickFindUF(N) }
+        });
+    }
 
     @Test
     public void test() {
-        final int n = 10;
-        final AssertableTestEntry[] assertableInput = new AssertableTestEntry[] {
-                new AssertableTestEntry(4, 3, false),
-                new AssertableTestEntry(3, 8, false),
-                new AssertableTestEntry(5, 6, false),
-                new AssertableTestEntry(9, 4, false),
-                new AssertableTestEntry(2, 1, false),
-                new AssertableTestEntry(8, 9, true),
-                new AssertableTestEntry(5, 0, false),
-                new AssertableTestEntry(7, 2, false),
-                new AssertableTestEntry(6, 1, false),
-                new AssertableTestEntry(0, 7, true),
-                new AssertableTestEntry(2, 5, true),
-                new AssertableTestEntry(8, 4, true),
-                new AssertableTestEntry(2, 3, false),
-                new AssertableTestEntry(0, 9, true),
-        };
-
-        final QuickFindUF uf = new QuickFindUF(n);
-        Arrays.stream(assertableInput).forEach(input -> {
+        Arrays.stream(ASSERTABLE_INPUT).forEach(input -> {
             final boolean connected = uf.connected(input.p, input.q);
             assertThat(input.connected, equalTo(connected));
             if (!connected) {
@@ -42,6 +59,9 @@ public class QuickFindUFTest {
         });
     }
 
+    /**
+     * @author volodymyr.tsukur
+     */
     private static class AssertableTestEntry {
 
         final int p;
