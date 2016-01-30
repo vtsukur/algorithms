c3860931d839221c0a74696ffe47a199b440a17b
glvtsuku <volodymyr.tsukur@avid.com>
2016-01-30 17:56:00 +0200

    Reflecting that existing weighted quick union algorithm uses height-based comparison, not size-based.

---
+++ src/main/java/org/vtsukur/algorithms/union_find/WeightedQuickUnionByHeightPathCompressionUF.java
@@ -0,0 +1,57 @@
+package org.vtsukur.algorithms.union_find;
+
+import java.util.Arrays;
+
+/**
+ * @author volodymyr.tsukur
+ */
+public final class WeightedQuickUnionByHeightPathCompressionUF extends AbstractUnionFind {
+
+    private final int[] depths;
+
+    public WeightedQuickUnionByHeightPathCompressionUF(final int n) {
+        super(n);
+        depths = new int[n];
+        Arrays.fill(depths, 1);
+    }
+
+    @Override
+    public void union(final int p, final int q) {
+        final int pRoot = root(p);
+        final int qRoot = root(q);
+
+        if (pRoot == qRoot) {
+            return;
+        }
+
+        if (depths[pRoot] == depths[qRoot]) {
+            store[pRoot] = qRoot;
+            depths[qRoot] += 1;
+        } else if (depths[pRoot] < depths[qRoot]) {
+            store[pRoot] = qRoot;
+        } else {
+            store[qRoot] = pRoot;
+        }
+    }
+
+    @Override
+    public boolean connected(final int p, final int q) {
+        return root(p) == root(q);
+    }
+
+    private int root(final int of) {
+        int i = of;
+        while (store[i] != i) {
+            int parent = store[i];
+            store[i] = store[parent];
+            i = parent;
+        }
+        return i;
+    }
+
+    @Override
+    protected String getName() {
+        return "weighted quick union (by height) with path compression";
+    }
+
+}
