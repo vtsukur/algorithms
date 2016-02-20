eeb1d9bb91cf51a0df180aeb32330f5c04af1998
glvtsuku <volodymyr.tsukur@avid.com>
2016-02-20 17:32:25 +0200

    Smart shrinking.

--- src/main/java/org/vtsukur/algorithms/stack/ArrayBasedStackOfStrings.java
+++ src/main/java/org/vtsukur/algorithms/stack/ArrayBasedStackOfStrings.java
@@ -13,10 +13,8 @@ public final class ArrayBasedStackOfStrings implements StackOfStrings {
 
     @Override
     public void push(final String item) {
-        if (size == array.length) { // resize
-            final String[] oldArray = array;
-            array = new String[size * 2];
-            System.arraycopy(oldArray, 0, array, 0, size);
+        if (size == array.length) {
+            resize(size * 2);
         }
 
         array[size] = item;
@@ -29,10 +27,21 @@ public final class ArrayBasedStackOfStrings implements StackOfStrings {
             throw new NoSuchElementException("Can't pop from empty stack");
         }
 
+        if (size == array.length / 4) {
+            resize(array.length / 2);
+        }
+
         --size;
         final String item = array[size];
         array[size] = null;
+
         return item;
+    }
+
+    private void resize(final int capacity) {
+        final String[] oldArray = array;
+        array = new String[capacity];
+        System.arraycopy(oldArray, 0, array, 0, size);
     }
 
     @Override
