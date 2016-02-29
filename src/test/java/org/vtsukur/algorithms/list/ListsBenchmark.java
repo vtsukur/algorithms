791405197d76f8adc562c050845c2692f76efca7
vtsukur <flushdia@gmail.com>
2016-02-29 23:28:21 +0200

    Benchmarking add/remove for array list and linked list.

--- src/test/java/org/vtsukur/algorithms/list/ListsBenchmark.java
+++ src/test/java/org/vtsukur/algorithms/list/ListsBenchmark.java
@@ -7,6 +7,8 @@ import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ListIterator;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
@@ -23,15 +25,31 @@ public class ListsBenchmark {
 
     private final ArrayList<Integer> arrayList = new ArrayList<>();
 
+    private final LinkedList<Integer> linkedList = new LinkedList<>();
+
     @Setup
     public void setup() {
-        IntStream.range(0, MAX).forEach(arrayList::add);
+        fillUp(arrayList);
+        fillUp(linkedList);
+    }
+
+    private static void fillUp(final List<Integer> list) {
+        IntStream.range(0, MAX).forEach(list::add);
     }
 
     @Benchmark
-    public void test() {
+    public void benchmarkArrayList() {
+        iterateAddRemove(arrayList);
+    }
+
+    @Benchmark
+    public void benchmarkLinkedList() {
+        iterateAddRemove(arrayList);
+    }
+
+    private static void iterateAddRemove(final List<Integer> list) {
         int j = 0;
-        for (final ListIterator<Integer> i = arrayList.listIterator(); i.hasNext(); ) {
+        for (final ListIterator<Integer> i = list.listIterator(); i.hasNext(); ) {
             final Integer value = i.next();
             if (j++ % 10 == 0) {
                 i.remove();
