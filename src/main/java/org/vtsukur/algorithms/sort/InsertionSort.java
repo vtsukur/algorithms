10eec3d17e8a08f6ebf1cb65f2be5d201cff5f73
glvtsuku <volodymyr.tsukur@avid.com>
2016-03-12 19:13:25 +0200

    Implementation of insertion sort algorithm.

---
+++ src/main/java/org/vtsukur/algorithms/sort/InsertionSort.java
@@ -0,0 +1,19 @@
+package org.vtsukur.algorithms.sort;
+
+/**
+ * @author volodymyr.tsukur
+ */
+public final class InsertionSort<T extends Comparable<T>> implements Sort<T> {
+
+    @Override
+    public void sort(final T[] array) {
+        for (int i = 1; i < array.length; ++i) {
+            int j = i;
+            while (j > 0 && SortUtils.less(array[j], array[j - 1])) {
+                SortUtils.swap(array, j, j - 1);
+                --j;
+            }
+        }
+    }
+
+}
--- src/test/java/org/vtsukur/algorithms/sort/SortTest.java
+++ src/test/java/org/vtsukur/algorithms/sort/SortTest.java
@@ -24,7 +24,8 @@ public class SortTest {
     @Parameterized.Parameters(name = "{0}")
     public static Collection<Object[]> strategies() {
         return Arrays.asList(new Object[][] {
-                { new SelectionSort<Integer>() }
+                { new SelectionSort<Integer>() },
+                { new InsertionSort<Integer>() }
         });
     }
 
