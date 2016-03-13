33e8fcd999d9b5d13dfbc82e21f0a295015753c4
vtsukur <flushdia@gmail.com>
2016-03-14 01:03:18 +0200

    Stub for sort-based shuffle and a first version of shuffling test.

---
+++ src/main/java/org/vtsukur/algorithms/shuffling/SortBasedShuffle.java
@@ -0,0 +1,13 @@
+package org.vtsukur.algorithms.shuffling;
+
+/**
+ * @author volodymyr.tsukur
+ */
+public final class SortBasedShuffle implements Shuffle {
+
+    @Override
+    public <T> void shuffle(final T[] array) {
+        // TODO
+    }
+
+}
---
+++ src/test/java/org/vtsukur/algorithms/shuffling/ShufflingTest.java
@@ -0,0 +1,68 @@
+package org.vtsukur.algorithms.shuffling;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * @author volodymyr.tsukur
+ */
+@RunWith(Parameterized.class)
+@RequiredArgsConstructor
+public class ShufflingTest {
+
+    private final @NonNull Shuffle shuffle;
+
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection<Object[]> strategies() {
+        return Arrays.asList(new Object[][] {
+                { new SortBasedShuffle() }
+        });
+    }
+
+    @Test
+    public void test() {
+        final Integer[] array = newRange(0, 5);
+        final List<Integer> reference = copyOfAsList(array);
+
+        shuffle.shuffle(array);
+
+        // TODO test is shuffling is really random
+
+        final Double[] probabilities = new Double[array.length + 1];
+        probabilities[array.length - 1] = 0.0;
+        probabilities[array.length] = 1.0;
+        for (int i = 0; i < array.length - 1; ++i) {
+            final Integer selection = array[i];
+            final int originalPosition = reference.indexOf(selection);
+            final double remainingLength = array.length - i;
+            final double pLowerBound = (originalPosition - i) / remainingLength;
+            final double pUpperBound = (originalPosition - i + 1) / remainingLength;
+            probabilities[i] = (pUpperBound + pLowerBound) / 2;
+            final Integer swapWith = reference.get(i);
+            reference.set(i, selection);
+            reference.set(originalPosition, swapWith);
+            System.out.println(probabilities[i]);
+        }
+    }
+
+    private static Integer[] newRange(final int startInclusive, final int endExclusive) {
+        return IntStream.range(startInclusive, endExclusive)
+                .mapToObj(Integer::valueOf)
+                .collect(Collectors.toList())
+                .toArray(new Integer[endExclusive - startInclusive]);
+    }
+
+    private static List<Integer> copyOfAsList(final Integer[] array) {
+        return Arrays.asList(Arrays.copyOf(array, array.length));
+    }
+
+}
