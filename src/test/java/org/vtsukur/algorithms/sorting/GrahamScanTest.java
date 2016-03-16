0bf7f096f5714155678469c158118c8d3bda886a
vtsukur <flushdia@gmail.com>
2016-03-16 22:09:53 +0200

    Stub for Graham scan test.

---
+++ src/test/java/org/vtsukur/algorithms/sorting/GrahamScanTest.java
@@ -0,0 +1,15 @@
+package org.vtsukur.algorithms.sorting;
+
+import org.junit.Test;
+
+/**
+ * @author volodymyr.tsukur
+ */
+public class GrahamScanTest {
+
+    @Test
+    public void test() {
+        // TODO
+    }
+
+}
