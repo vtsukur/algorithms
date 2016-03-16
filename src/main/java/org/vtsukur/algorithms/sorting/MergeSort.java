65ee239abd4eeeae0e6f9dd247a29e53e03fa82d
vtsukur <flushdia@gmail.com>
2016-03-16 23:25:24 +0200

    Refactoring implementation of merge sort algorithm to avoid duplication by using a Worker instance. Preparatory work for reusable auxiliary array.

--- src/main/java/org/vtsukur/algorithms/sorting/MergeSort.java
+++ src/main/java/org/vtsukur/algorithms/sorting/MergeSort.java
@@ -2,6 +2,7 @@ package org.vtsukur.algorithms.sorting;
 
 import org.vtsukur.algorithms.util.ComparableUtils;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /**
@@ -11,41 +12,61 @@ public final class MergeSort extends BaseSort {
 
     @Override
     public <T extends Comparable<T>> void sort(final T[] array) {
-        doSort(array, 0, array.length - 1);
+        new Worker<>(array).sort();
     }
 
-    private <T extends Comparable<T>> void doSort(final T[] array, final int from, final int to) {
-        final int size = to - from + 1;
-        if (size <= 1) {
-            return;
+    /**
+     * @author volodymyr.tsukur
+     */
+    private static class Worker<T extends Comparable<T>> {
+
+        final T[] array;
+
+        final T[] aux;
+
+        Worker(final T[] array) {
+            this.array = array;
+            //noinspection unchecked
+            aux = (T[]) Array.newInstance(array.getClass().getComponentType(), array.length);
         }
 
-        final int middle = from + size / 2;
-        doSort(array, from, middle - 1);
-        doSort(array, middle, to);
-        merge(array, from, middle, to, size);
-    }
+        void sort() {
+            doSort(0, array.length - 1);
+        }
 
-    private <T extends Comparable<T>> void merge(final T[] array, final int from, final int middle, final int to,
-            final int size) {
-        final int part1Size = middle - from;
+        void doSort(final int from, final int to) {
+            final int size = to - from + 1;
+            if (size <= 1) {
+                return;
+            }
 
-        final T[] cpy = Arrays.copyOfRange(array, from, to + 1);
-        int m = from;
-        int i = 0, j = part1Size;
-        while (i < part1Size && j < size) {
-            if (ComparableUtils.less(cpy[i], cpy[j])) {
+            final int middle = from + size / 2;
+            doSort(from, middle - 1);
+            doSort(middle, to);
+            merge(from, middle, to, size);
+        }
+
+        void merge(final int from, final int middle, final int to, final int size) {
+            final int part1Size = middle - from;
+
+            final T[] cpy = Arrays.copyOfRange(array, from, to + 1);
+            int m = from;
+            int i = 0, j = part1Size;
+            while (i < part1Size && j < size) {
+                if (ComparableUtils.less(cpy[i], cpy[j])) {
+                    array[m++] = cpy[i++];
+                } else {
+                    array[m++] = cpy[j++];
+                }
+            }
+            while (i < part1Size) {
                 array[m++] = cpy[i++];
-            } else {
+            }
+            while (j < size) {
                 array[m++] = cpy[j++];
             }
         }
-        while (i < part1Size) {
-            array[m++] = cpy[i++];
-        }
-        while (j < size) {
-            array[m++] = cpy[j++];
-        }
+
     }
 
 }
