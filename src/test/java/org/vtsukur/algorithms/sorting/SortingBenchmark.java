3e485cc4d5b6d61d9e3a1d533f7a45b8a833db37
glvtsuku <volodymyr.tsukur@avid.com>
2016-03-19 19:17:19 +0200

    Adding benchmark for home-grown merge sort vs Java Arrays' sort.

---
+++ src/test/java/org/vtsukur/algorithms/sorting/SortingBenchmark.java
@@ -0,0 +1,71 @@
+package org.vtsukur.algorithms.sorting;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.lang.invoke.MethodHandles.lookup;
+
+/**
+ * @author volodymyr.tsukur
+ */
+@State(Scope.Benchmark)
+public class SortingBenchmark {
+
+    private static final int SIZE = 1_000_000;
+
+    private static final Random RANDOM = new Random();
+
+    private static final MergeSort MERGE_SORT = new MergeSort();
+
+    @Benchmark
+    public void benchmarkMergeSort() {
+        MERGE_SORT.sort(generateArrayOfNRandomItems());
+    }
+
+    @Benchmark
+    public void benchmarkJavaArraysSort() {
+        Arrays.sort(generateArrayOfNRandomItems());
+    }
+
+    private static Integer[] generateArrayOfNRandomItems() {
+        final List<Integer> generated = IntStream.generate(RANDOM::nextInt)
+                .limit(SIZE)
+                .mapToObj(Integer::valueOf)
+                .collect(Collectors.toList());
+        return generated.toArray(new Integer[generated.size()]);
+    }
+
+    public static void main(final String[] args) throws RunnerException {
+        new Runner(options()).run();
+    }
+
+    private static Options options() {
+        return new OptionsBuilder()
+                .include(thisClassName())
+                .forks(1)
+                .threads(1)
+                .timeUnit(TimeUnit.MILLISECONDS)
+                .mode(Mode.AverageTime)
+                .warmupIterations(10)
+                .measurementIterations(10)
+                .build();
+    }
+
+    private static String thisClassName() {
+        return lookup().lookupClass().getSimpleName();
+    }
+
+}
