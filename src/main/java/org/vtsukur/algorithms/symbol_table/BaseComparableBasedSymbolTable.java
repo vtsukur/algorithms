0ea5d721d1b941515d5c755a48477f4708c38133
glvtsuku <volodymyr.tsukur@avid.com>
2016-03-23 22:31:19 +0200

    Enriching base implementation of Comparable-based symbol table with a simple `isEmpty` check, asserion utility for the keys and human-centric `toString` implementation.

--- src/main/java/org/vtsukur/algorithms/symbol_table/BaseComparableBasedSymbolTable.java
+++ src/main/java/org/vtsukur/algorithms/symbol_table/BaseComparableBasedSymbolTable.java
@@ -1,5 +1,9 @@
 package org.vtsukur.algorithms.symbol_table;
 
+import org.vtsukur.algorithms.util.StringUtils;
+
+import java.util.Objects;
+
 /**
  * @author volodymyr.tsukur
  */
@@ -13,6 +17,20 @@ abstract class BaseComparableBasedSymbolTable<K extends Comparable<K>, V> implem
     @Override
     public final void delete(final K key) {
         put(key, null);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    protected final void assertKey(final K key) {
+        Objects.requireNonNull(key, "key is null");
+    }
+
+    @Override
+    public String toString() {
+        return StringUtils.humanizeClassNameLowerCase(getClass());
     }
 
 }
