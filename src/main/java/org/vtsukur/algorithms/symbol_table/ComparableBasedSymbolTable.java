eff3cacab01be96205d214ef30bfacc7c6b0f9b2
glvtsuku <volodymyr.tsukur@avid.com>
2016-03-23 21:26:50 +0200

    Making keys in symbol table Comparable to enable more efficient implementations based on key comparison and to support broader set of operations. Changing order of operations so that `containsKey` goes first and `delete` goes afterwards.

--- src/main/java/org/vtsukur/algorithms/symbol_table/ComparableBasedSymbolTable.java
+++ src/main/java/org/vtsukur/algorithms/symbol_table/ComparableBasedSymbolTable.java
@@ -3,15 +3,15 @@ package org.vtsukur.algorithms.symbol_table;
 /**
  * @author volodymyr.tsukur
  */
-public interface SymbolTable<K, V> {
+public interface ComparableBasedSymbolTable<K extends Comparable<K>, V> {
 
     void put(K key, V value);
 
     V get(K key);
 
-    void delete(K key);
-
     boolean contains(K key);
+
+    void delete(K key);
 
     boolean isEmpty();
 
