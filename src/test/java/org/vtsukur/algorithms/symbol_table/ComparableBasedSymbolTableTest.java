7fee4a3c990bb2e950ec6776267dfe7f5bd6f082
glvtsuku <volodymyr.tsukur@avid.com>
2016-03-23 22:42:35 +0200

    More tests!

--- src/test/java/org/vtsukur/algorithms/symbol_table/ComparableBasedSymbolTableTest.java
+++ src/test/java/org/vtsukur/algorithms/symbol_table/ComparableBasedSymbolTableTest.java
@@ -3,11 +3,14 @@ package org.vtsukur.algorithms.symbol_table;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.openjdk.jmh.annotations.Setup;
 
 import java.util.Arrays;
 import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author volodymyr.tsukur
@@ -28,10 +31,39 @@ public class ComparableBasedSymbolTableTest {
         });
     }
 
+    @Setup
+    public void clear() {
+        strategy.clear();
+    }
+
     @Test
-    public void test() {
+    public void put_with_positive_get_and_contains() {
         strategy.put("is", 1);
         assertEquals(strategy.get("is"), Integer.valueOf(1));
+        assertTrue(strategy.contains("is"));
+        assertFalse(strategy.isEmpty());
+        assertEquals(strategy.size(), 1);
+    }
+
+    @Test
+    public void put_null_value_is_the_same_as_delete() {
+        strategy.put("is", 1);
+        assertEquals(strategy.get("is"), Integer.valueOf(1));
+
+        strategy.put("is", null);
+        assertEquals(strategy.get("is"), null);
+
+        strategy.put("is", 1);
+        assertEquals(strategy.get("is"), Integer.valueOf(1));
+
+        strategy.delete("is");
+        assertEquals(strategy.get("is"), null);
+    }
+
+    @Test
+    public void empty_case() {
+        assertTrue(strategy.isEmpty());
+        assertEquals(strategy.size(), 0);
     }
 
 }
